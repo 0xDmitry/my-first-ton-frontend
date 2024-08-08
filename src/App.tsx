7a3c8f8486c20e38621987b3bb699f2d39139f19
0xDmitry <sign-69@mail.ru>
2024-08-08 15:56:31 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { fromNano } from "@ton/core";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import { useMainContract } from "./hooks/useMainContract";
+import { useTonConnect } from "./hooks/useTonConnect";
+
+import "./App.css";
+
+function App() {
+  const {
+    contract_address,
+    counter_value,
+    recent_sender,
+    owner_address,
+    contract_balance,
+    sendIncrement,
+    sendDeposit,
+    sendWithdrawalRequest,
+  } = useMainContract();
+  const { connected } = useTonConnect()
+
+  return (
+    <div>
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className='Card'>
+          <b>Contract Address</b>
+          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+          <b>Contract Balance</b>
+          <div className='Hint'>{contract_balance ? fromNano(contract_balance) : undefined}</div>
+        </div>
+
+        <div className='Card'>
+          <b>Owner Address</b>
+          <div>{owner_address?.slice(0, 30) + "..."}</div>
+          <b>Counter Value</b>
+          <div>{counter_value}</div>
+          <b>Recent Sender</b>
+          <div>{recent_sender?.slice(0, 30) + "..."}</div>
+        </div>
+
+        {connected && (
+          <>
+            <a
+              onClick={() => {
+                sendIncrement(5);
+              }}
+            >
+              Increment by 5
+            </a>
+            <br/>
+            <a
+              onClick={() => {
+                sendDeposit(1);
+              }}
+            >
+              Deposit 1 TON
+            </a>
+            <br/>
+            <a
+              onClick={() => {
+                sendWithdrawalRequest(1);
+              }}
+            >
+              Withdraw 1 TON
+            </a>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useMainContract.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { Address, OpenedContract, toNano } from "@ton/core";
+import { MainContract } from "../contracts/MainContract";
+import { useTonClient } from "./useTonClient";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { useTonConnect } from "./useTonConnect";
+import { Contract } from "ton";
+
+export function useMainContract() {
+  const client = useTonClient();
+  const { sender } = useTonConnect();
+
+  const [contractData, setContractData] = useState<null | {
+    counter_value: number;
+    recent_sender: string;
+    owner_address: string;
+  }>();
+  const [balance, setBalance] = useState<null | number>();
+
+  const mainContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new MainContract(
+      Address.parse("EQAIx0wvh_GQCZNYspzUh7AiRM9MT-f5n7z0Ow2-D2OW5T2b")
+    );
+    return client.open(contract as Contract) as OpenedContract<MainContract>;
+  }, [client]);
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
+  useEffect(() => {
+    async function getValue() {
+      if (!mainContract) return;
+      setContractData(null);
+      const dataResponse = await mainContract.getData();
+      const balanceResponce = await mainContract.getBalance()
+      setContractData({
+        counter_value: dataResponse.number,
+        recent_sender: dataResponse.recent_sender.toString(),
+        owner_address: dataResponse.owner_address.toString(),
+      });
+      setBalance(balanceResponce.number)
+
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
+    }
+    getValue();
+  }, [mainContract]);
+
+  return {
+    ...contractData,
+    contract_address: mainContract?.address.toString(),
+    contract_balance: balance,
+    sendIncrement: (value: number) => {
+      return mainContract?.sendIncrement(sender, toNano(0.05), value);
+    },
+    sendDeposit: (value: number) => {
+      return mainContract?.sendDeposit(sender, toNano(value));
+    },
+    sendWithdrawalRequest: (value: number) => {
+      return mainContract?.sendWithdrawalRequest(sender, toNano(0.05), toNano(value));
+    }
+  };
+}
