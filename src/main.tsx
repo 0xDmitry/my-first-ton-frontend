9b54c1f33933f507bab86295391de89f3376098b
0xDmitry <sign-69@mail.ru>
2024-08-09 13:58:18 +0400

    Change manifest

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import App from './App';
 
 import './index.css';
 
-// this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://0xdmitry.github.io/first_contract_front_end/public/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
